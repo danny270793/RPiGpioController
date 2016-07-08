00342db35e4e880ea9cf58097194a8d211e05a29
Danny <danny270793@gmail.com>
2016-07-08 01:42:27 -0500

    Get information about your Raspberry

---
+++ php/getSystemInfo.php
@@ -0,0 +1,21 @@
+<?php
+	$isset_user=isset($_POST["app_user"]);
+  $isset_password=isset($_POST["app_password"]);
+  $response=array();
+  if($isset_user&&$isset_password){
+		$valid_user=$_POST["app_user"]=="danny";
+    $valid_password=$_POST["app_password"]=="270793";
+    if($valid_user&&$valid_password){
+			$response=exec("sudo python /var/www/html/RPiGpioController/pyton/getSystemInfo.py");
+			print($response);
+		}else{
+			$response["state"]="2";
+			$response["message"]="Invalid credentials.";
+			print(json_encode($response));
+		}
+	}else{
+		$response["state"]="0";
+		$response["message"]="No credetials send.";
+		print(json_encode($response));
+	}
+?>
