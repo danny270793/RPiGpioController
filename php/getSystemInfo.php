7f53eaf4b0e775a828232a72c5748cea1d288fe2
Danny <danny270793@gmail.com>
2016-09-04 13:40:47 -0500

    Version 2.4.9

---
+++ php/getSystemInfo.php
@@ -0,0 +1,22 @@
+<?php
+	require_once("info.php");
+	$isset_user=isset($_POST["app_user"]);
+	$isset_password=isset($_POST["app_password"]);
+	$response=array();
+	if($isset_user&&$isset_password){
+		$valid_user=$_POST["app_user"]==$user;
+		$valid_password=$_POST["app_password"]==$password;
+		if($valid_user&&$valid_password){
+			$response=exec("sudo python /var/www/html/RPiGpioController/pyton/getSystemInfo.py");
+			print($response);
+		}else{
+			$response["state"]="2";
+			$response["message"]="Invalid credentials.";
+			print(json_encode($response));
+		}
+	}else{
+		$response["state"]="0";
+		$response["message"]="No credetials send.";
+		print(json_encode($response));
+	}
+?>
---
+++ php/info.php
@@ -0,0 +1,4 @@
+<?php
+	$user="danny";
+	$password="270793";
+?>
