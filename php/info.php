fe27a0223dff2ca8fa2c32076f7f6908c55526ac
Danny <danny270793@gmail.com>
2016-09-15 22:56:30 -0500

    list of variables

--- php/info.php
+++ php/info.php
@@ -1,4 +1,30 @@
 <?php
-	$user="admin";
-	$password="admin";
+	/* Access credentials */
+		$user="admin";
+		$password="admin";
+
+	/* Json responses */
+		$state="state";
+		$message="message";
+
+	/* Json responses values */
+		$invalidCredentials=2;
+		$invalidCredentialsMessage="Invalid Credentials";
+		$noCredetialsSend=0;
+		$noCredetialsSendMessage="No credentials send";
+		$userLogged=1;
+		$userLoggedMessage="User logged";
+
+	/* Post data */
+		$appUser="app_user";
+		$appPassword="app_password";
+		$pin="pin";
+		$pinState="state";
+
+	/* Url */
+		$getSystemInfoUrl="sudo python /var/www/html/RPiGpioController/pyton/getSystemInfo.py";
+		$readUrl="sudo python /var/www/html/RPiGpioController/pyton/read.py";
+		$writeUrl="sudo python /var/www/html/RPiGpioController/pyton/write.py";
+		$rebootUrl="sudo python /var/www/html/RPiGpioController/pyton/reboot.py";
+		$shutdownUrl="sudo python /var/www/html/RPiGpioController/pyton/shutdown.py";
 ?>
