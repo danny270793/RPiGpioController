99a5e29d924b74d6411bc2bf9cd4570b9e474268
Danny <danny270793@gmail.com>
2016-09-16 12:43:11 -0500

    write pin state

---
+++ python/write.py
@@ -0,0 +1,24 @@
+from info import *
+import json,sys
+# check arguments
+if not len(sys.argv)==3:
+    # invalid data size
+    print json.dumps({"state":invalidArgumentsCode,"message":"invalid arguments number"})
+else:
+    # get data
+    pinNumber=int(sys.argv[1])
+    mode=sys.argv[2]
+    
+    # create pin and set mode
+    try:
+        from Gpio import *
+        pin=Pin(pinNumber)
+        pin.setMode(OUTPUT)
+        if mode=='high':
+            pin.setState(pin.HIGH)
+            print json.dumps({"state":okCode,"message":"Pin set to high correctly"})
+        if mode=='low':
+            pin.setState(pin.LOW)
+            print json.dumps({"state":okCode,"message":"Pin set to low correctly"})
+    except Exception as exception:
+        print json.dumps({"state":exceptionCode,"message":exception})
